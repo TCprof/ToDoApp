15271e1560287c38322db5fe50898f7e4128de73
TCprof <tomcordall@gmail.com>
2020-08-29 16:19:07 +0100

    added creation component

---
+++ frontend/components/ToDoCreate.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="create-container">
+    <input v-model="contentOfToDo" type="text">
+    <button @click="create">
+      Add
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      contentOfToDo: ''
+    }
+  },
+  methods: {
+    create () {
+      this.$services.todo.create(this.contentOfToDo).then((data) => {
+        this.$emit('create', data)
+        this.contentOfToDo = ''
+      })
+    }
+  }
+}
+</script>
